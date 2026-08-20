2d9617570fb495eb7758333d32e4b816ae06ae2d
Shakil Ahmed <sa3akash@gmail.com>
2026-08-21 03:09:35 +0600

    fix(ci): resolve reanimated cmake target error and jest immer module mapper

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-reanimated|react-native-mmkv|immer)/)',
+  ],
+  moduleNameMapper: {
+    '^immer$': '<rootDir>/node_modules/immer/dist/cjs/index.js',
+  },
 };
